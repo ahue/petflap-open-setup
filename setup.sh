701968726c91a5864b7d5f87dcad28a18fddbc69
A. Hue <ahue87@gmail.com>
2020-06-11 13:33:41 +0200

    initial commit

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd ~
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Install git
+sudo apt-get install git 
+
+# Install postgres
+sudo apt-get install postgresql libpq-dev postgresql-client postgresql-client-common -y
+
+# Install redis
+sudo apt-get install redis -y
+
+# Install motion
+sudo apt-get install motion -y
+
+# Install mqtt publisher/subscriber
+sudo apt-get install mosquitto-clients -y
+
+# Install node-red
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+sudo systemctl enable nodered.service
+
+# Clone the repository
+git clone --bare https://github.com/ahue/open-petflap-wiki.git
