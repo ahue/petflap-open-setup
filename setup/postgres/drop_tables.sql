01a278392a94722a4d5c232ebe75a07c9eb3b8d9
Andreas Huebner <ahue87@gmail.com>
2020-07-02 22:24:47 +0200

    changed drop table

--- setup/postgres/drop_tables.sql
+++ setup/postgres/drop_tables.sql
@@ -1,2 +1,6 @@
-select 'drop table if exists "' || tablename || '" cascade;' from pg_tables
- where schemaname = 'public'; -- or any other schema
+drop table if exists "motion" cascade;
+drop table if exists "motion_log" cascade;
+drop table if exists "security" cascade;
+drop table if exists "user" cascade;
+drop table if exists "pet" cascade;
+drop table if exists "passage" cascade;
