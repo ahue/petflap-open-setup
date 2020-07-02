106e41e81e14abf357727f965321468bed2362ca
Andreas Huebner <ahue87@gmail.com>
2020-07-02 22:05:29 +0200

    Better script

--- setup/postgres/create_petflap.sql
+++ setup/postgres/create_petflap.sql
@@ -1,91 +1,256 @@
--- --------------------------------------------------------
--- Host:                         192.168.0.82
--- Server Version:               PostgreSQL 11.7 (Raspbian 11.7-0+deb10u1) on arm-JSONB-linux-gnueabihf, compiled by gcc (Raspbian 8.3.0-6+rpi1) 8.3.0, 32-bit
--- Server Betriebssystem:        
--- HeidiSQL Version:             11.0.0.5919
--- --------------------------------------------------------
+--
+-- PostgreSQL database dump
+--
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES  */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- Dumped from database version 11.7 (Raspbian 11.7-0+deb10u1)
+-- Dumped by pg_dump version 12.2
 
--- Exportiere Struktur von Tabelle public.motion
-DROP TABLE IF EXISTS "motion";
-CREATE TABLE IF NOT EXISTS "motion" (
-	"event_id" BIGINT NOT NULL,
-	"timestamp_local" TIMESTAMP NULL DEFAULT NULL,
-	"timestamp_utc" BIGINT NULL DEFAULT NULL,
-	"doc" JSONB NULL DEFAULT NULL,
-	PRIMARY KEY ("event_id")
+-- Started on 2020-07-02 22:02:43
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 204 (class 1259 OID 16506)
+-- Name: motion; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.motion (
+    event_id bigint NOT NULL,
+    timestamp_local timestamp without time zone,
+    timestamp_utc bigint,
+    doc jsonb DEFAULT '{}'::jsonb
 );
 
--- Daten Export vom Benutzer nicht ausgewählt
 
--- Exportiere Struktur von Tabelle public.motion_log
-DROP TABLE IF EXISTS "motion_log";
-CREATE TABLE IF NOT EXISTS "motion_log" (
-	"event_id" BIGINT NOT NULL,
-	"camera" INTEGER NULL DEFAULT NULL,
-	"filename" CHAR(80) NOT NULL,
-	"frame" INTEGER NULL DEFAULT NULL,
-	"file_type" INTEGER NULL DEFAULT NULL,
-	"time_stamp" TIMESTAMP NULL DEFAULT NULL,
-	"changed_pixels" INTEGER NULL DEFAULT NULL,
-	"noise_level" INTEGER NULL DEFAULT NULL,
-	"motion_area_h" INTEGER NULL DEFAULT NULL,
-	"motion_area_w" INTEGER NULL DEFAULT NULL,
-	"motion_center_x" INTEGER NULL DEFAULT NULL,
-	"motion_center_y" INTEGER NULL DEFAULT NULL
+--
+-- TOC entry 2942 (class 0 OID 0)
+-- Dependencies: 204
+-- Name: COLUMN motion.timestamp_utc; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.motion.timestamp_utc IS 'milliseconds';
+
+
+--
+-- TOC entry 203 (class 1259 OID 16485)
+-- Name: motion_log; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.motion_log (
+    event_id bigint NOT NULL,
+    camera integer,
+    filename character(80) NOT NULL,
+    frame integer,
+    file_type integer,
+    time_stamp timestamp without time zone,
+    changed_pixels integer,
+    noise_level integer,
+    motion_area_h integer,
+    motion_area_w integer,
+    motion_center_x integer,
+    motion_center_y integer
 );
 
--- Daten Export vom Benutzer nicht ausgewählt
 
--- Exportiere Struktur von Tabelle public.passage
-DROP TABLE IF EXISTS "passage";
-CREATE TABLE IF NOT EXISTS "passage" (
-	"doc" JSONB NOT NULL,
-	"id" INTEGER NOT NULL DEFAULT 'nextval(''passage_id_seq''::regclass)',
-	"pet" INTEGER NULL DEFAULT NULL,
-	PRIMARY KEY ("id"),
-	CONSTRAINT "pet" FOREIGN KEY ("pet") REFERENCES "public"."pet" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION
+--
+-- TOC entry 198 (class 1259 OID 16410)
+-- Name: passage; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.passage (
+    doc jsonb NOT NULL,
+    id integer NOT NULL,
+    pet integer
 );
 
--- Daten Export vom Benutzer nicht ausgewählt
 
--- Exportiere Struktur von Tabelle public.pet
-DROP TABLE IF EXISTS "pet";
-CREATE TABLE IF NOT EXISTS "pet" (
-	"doc" JSONB NOT NULL,
-	"id" INTEGER NOT NULL DEFAULT 'nextval(''pet_id_seq''::regclass)',
-	PRIMARY KEY ("id")
+--
+-- TOC entry 199 (class 1259 OID 16419)
+-- Name: passage_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.passage_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 2943 (class 0 OID 0)
+-- Dependencies: 199
+-- Name: passage_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.passage_id_seq OWNED BY public.passage.id;
+
+
+--
+-- TOC entry 197 (class 1259 OID 16399)
+-- Name: pet; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.pet (
+    doc jsonb NOT NULL,
+    id integer NOT NULL
 );
 
--- Daten Export vom Benutzer nicht ausgewählt
 
--- Exportiere Struktur von Tabelle public.security
-DROP TABLE IF EXISTS "security";
-CREATE TABLE IF NOT EXISTS "security" (
-	"camera" INTEGER NULL DEFAULT NULL,
-	"filename" CHAR(80) NOT NULL,
-	"frame" INTEGER NULL DEFAULT NULL,
-	"file_type" INTEGER NULL DEFAULT NULL,
-	"time_stamp" TIMESTAMP NULL DEFAULT NULL,
-	"event_time_stamp" TIMESTAMP NULL DEFAULT NULL
+--
+-- TOC entry 200 (class 1259 OID 16430)
+-- Name: pet_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.pet_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 2944 (class 0 OID 0)
+-- Dependencies: 200
+-- Name: pet_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.pet_id_seq OWNED BY public.pet.id;
+
+
+--
+-- TOC entry 202 (class 1259 OID 16464)
+-- Name: security; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.security (
+    camera integer,
+    filename character(80) NOT NULL,
+    frame integer,
+    file_type integer,
+    time_stamp timestamp without time zone,
+    event_time_stamp timestamp without time zone
 );
 
--- Daten Export vom Benutzer nicht ausgewählt
 
--- Exportiere Struktur von Tabelle public.user
-DROP TABLE IF EXISTS "user";
-CREATE TABLE IF NOT EXISTS "user" (
-	"doc" JSONB NOT NULL,
-	"id" INTEGER NOT NULL DEFAULT 'nextval(''user_id_seq''::regclass)',
-	PRIMARY KEY ("id")
+--
+-- TOC entry 196 (class 1259 OID 16388)
+-- Name: user; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public."user" (
+    doc jsonb NOT NULL,
+    id integer NOT NULL
 );
 
--- Daten Export vom Benutzer nicht ausgewählt
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+--
+-- TOC entry 201 (class 1259 OID 16441)
+-- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 2945 (class 0 OID 0)
+-- Dependencies: 201
+-- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
+
+
+--
+-- TOC entry 2805 (class 2604 OID 16421)
+-- Name: passage id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.passage ALTER COLUMN id SET DEFAULT nextval('public.passage_id_seq'::regclass);
+
+
+--
+-- TOC entry 2804 (class 2604 OID 16432)
+-- Name: pet id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.pet ALTER COLUMN id SET DEFAULT nextval('public.pet_id_seq'::regclass);
+
+
+--
+-- TOC entry 2803 (class 2604 OID 16443)
+-- Name: user id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
+
+
+--
+-- TOC entry 2814 (class 2606 OID 16513)
+-- Name: motion motion_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.motion
+    ADD CONSTRAINT motion_pkey PRIMARY KEY (event_id);
+
+
+--
+-- TOC entry 2812 (class 2606 OID 16429)
+-- Name: passage passage_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.passage
+    ADD CONSTRAINT passage_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2810 (class 2606 OID 16440)
+-- Name: pet pet_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.pet
+    ADD CONSTRAINT pet_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2808 (class 2606 OID 16451)
+-- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public."user"
+    ADD CONSTRAINT user_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2815 (class 2606 OID 16452)
+-- Name: passage pet; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.passage
+    ADD CONSTRAINT pet FOREIGN KEY (pet) REFERENCES public.pet(id) NOT VALID;
+
+
+-- Completed on 2020-07-02 22:02:46
+
+--
+-- PostgreSQL database dump complete
+--
+
